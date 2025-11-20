3dea60dd96a4b7fbf3988efdebdfabf523159e29
Marcelo Torres <marcelotorres1988@hotmail.com>
2025-11-19 23:46:02 -0300

    test 2

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Botón de saludo
 document.getElementById("btnSaludo").addEventListener("click", () => {
-  document.getElementById("mensaje").textContent = "¡Funciona perfecto en tu Poco X3 Pro!";
-});
-
-// Carrusel
-const track = document.querySelector(".carousel-track");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-let index = 0;
-
-nextBtn.addEventListener("click", () => {
-  index = (index + 1) % track.children.length;
-  track.style.transform = `translateX(-${index * 300}px)`;
-});
-
-prevBtn.addEventListener("click", () => {
-  index = (index - 1 + track.children.length) % track.children.length;
-  track.style.transform = `translateX(-${index * 300}px)`;
+  document.getElementById("mensaje").textContent = "¡Funciona perfecto en tu Poco X3 Pro con Bootstrap!";
 });
